d2ff6020b16a5f831b03cca5a1ec8917f1423172
capYousef <elcaptain.yousef.official@gmail.com>
2023-12-18 20:11:30 +0200

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,7 +32,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
             Route::middleware('web')
-                ->prefix('dash')
+                ->prefix('dashboard')
                 ->group(base_path('routes/dashboard.php'));
         });
     }
